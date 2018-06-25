ddaaf92e4f2fa63458aa03e40d2de7769f74d4de
ismail-creatvt <ismailshaikh31097@gmail.com>
2018-06-25 11:07:47 +0530

    Added try catch block and spinner to select modes

--- app/src/main/java/com/creatvt/ismail/webserviceexample/activity/AppConstant.java
+++ app/src/main/java/com/creatvt/ismail/webserviceexample/activity/AppConstant.java
@@ -5,6 +5,6 @@ public class AppConstant {
     public static final String BASE_URL = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=";
     public static final String DIRECTION_API_KEY = "AIzaSyCEq9sfQp6-nvD95D8mKj4ffTZv74s92Cw";
     public static final String DESTINATION = "&destinations=";
-    public static final String MODE = "&mode=walking";
+    public static final String MODE = "&mode=";
     public static final String ALTERNATIVES = "&alternatives=false";
 }
--- app/src/main/java/com/creatvt/ismail/webserviceexample/activity/RouteDistance.java
+++ app/src/main/java/com/creatvt/ismail/webserviceexample/activity/RouteDistance.java
@@ -1,19 +1,29 @@
 package com.creatvt.ismail.webserviceexample.activity;
 
+import android.content.DialogInterface;
 import android.graphics.Path;
 import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatSpinner;
 import android.support.v7.widget.CardView;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.creatvt.ismail.webserviceexample.R;
 import com.creatvt.ismail.webserviceexample.data.DirectionResponse;
+import com.creatvt.ismail.webserviceexample.data.ElementsItem;
 import com.creatvt.ismail.webserviceexample.service.APIClient;
 import com.creatvt.ismail.webserviceexample.service.APIInterface;
 
@@ -21,12 +31,13 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class RouteDistance extends AppCompatActivity {
+public class RouteDistance extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
-    TextInputEditText txtSource,txtDestination;
-    TextView txtDistance,txtDuration;
-    CardView searchBtn;
-    ProgressBar progress;
+    private TextInputEditText txtSource,txtDestination;
+    private TextView txtDistance,txtDuration;
+    private CardView searchBtn;
+    private ProgressBar progress;
+    private Spinner travelMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +52,12 @@ public class RouteDistance extends AppCompatActivity {
 
         searchBtn = findViewById(R.id.search_button);
         progress =findViewById(R.id.progress_bar);
+
+        travelMode = (Spinner) findViewById(R.id.travel_mode);
+        ArrayAdapter<CharSequence> travelModes = ArrayAdapter.createFromResource(this,R.array.travel_modes,android.R.layout.simple_spinner_item);
+        travelModes.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        travelMode.setOnItemSelectedListener(this);
+        travelMode.setAdapter(travelModes);
 
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,8 +77,9 @@ public class RouteDistance extends AppCompatActivity {
 
                 String source = txtSource.getText().toString().trim();
                 String destination = txtDestination.getText().toString().trim();
-
-                String url = AppConstant.BASE_URL + source + AppConstant.DESTINATION + destination + AppConstant.MODE + AppConstant.ALTERNATIVES + "&key=" + AppConstant.DIRECTION_API_KEY;
+                String selected = travelMode.getSelectedItem().toString();
+                Log.i("SELECTED ITEM",selected);
+                String url = AppConstant.BASE_URL + source + AppConstant.DESTINATION + destination + AppConstant.MODE + selected + AppConstant.ALTERNATIVES + "&key=" + AppConstant.DIRECTION_API_KEY;
 
                 APIInterface service = APIClient.getClient().create(APIInterface.class);
 
@@ -72,13 +90,34 @@ public class RouteDistance extends AppCompatActivity {
                     public void onResponse(Call<DirectionResponse> call, Response<DirectionResponse> response) {
                         DirectionResponse direction = response.body();
 
-                        String distance = direction.getRows().get(0).getElements().get(0).getDistance().getText();
-                        String duration = direction.getRows().get(0).getElements().get(0).getDuration().getText();
+                        ElementsItem element = direction.getRows().get(0).getElements().get(0);
 
-                        txtDistance.setText(distance);
-                        txtDuration.setText(duration);
+                        try {
+                            String distance = element.getDistance().getText();
+                            String duration = element.getDuration().getText();
 
-                        progress.setVisibility(View.GONE);
+                            txtDistance.setText(distance);
+                            txtDuration.setText(duration);
+
+                        }
+                        catch(NullPointerException npe){
+                            AlertDialog.Builder builder = new AlertDialog.Builder(RouteDistance.this);
+                            builder.setTitle("Not Found");
+                            builder.setMessage("Please Enter Valid Source and Destination");
+                            builder.setCustomTitle(LayoutInflater.from(RouteDistance.this).inflate(R.layout.not_found_title_layout,null,true));
+                            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                }
+                            });
+                            AlertDialog alert = builder.create();
+                            alert.show();
+                        }
+                        finally {
+                            progress.setVisibility(View.GONE);
+                        }
+
                     }
 
                     @Override
@@ -95,5 +134,15 @@ public class RouteDistance extends AppCompatActivity {
         Toast toast = Toast.makeText(this,msg,Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.TOP,0,200);
         toast.show();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        showToast("Please Select Travel Mode...");
     }
 }
